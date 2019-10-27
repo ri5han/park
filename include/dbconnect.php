a2613efb1877e7c4cacdfe8b2211f082ddb3d940
ri5han <rishanmascarenhas@gmail.com>
2019-10-27 17:36:55 +0530

    bug fix

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -2,7 +2,7 @@
 
     $conn = mysqli_connect('localhost','root','','park');
     if(mysqli_connect_errno()) {
-        echo "Failed to connect db";
+        echo "<script>alert('Failed to connect db');</script>";
     } else {
         //echo "Connected to db";
     }
